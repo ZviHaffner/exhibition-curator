910f37367de7e00871bd005478ee2d3cb28771ad
Zvi Haffner <zvihaffner@gmail.com>
2025-01-13 18:42:02 +0000

    Adapts results count for both APIs

--- src/components/ArtworkList.jsx
+++ src/components/ArtworkList.jsx
@@ -2,31 +2,43 @@ import ArtworkCard from "@/components/ArtworkCard";
 import PaginationBar from "@/components/PaginationBar";
 
 const ArtworkList = ({
+  selectedApi,
   searchTerm,
   artworks,
   setArtworks,
   setLoading,
   setError,
 }) => {
+  function renderResultsCount() {
+    let middleKey;
+    if (selectedApi === "artInstChicago") {
+      middleKey = "pagination";
+    }
+    if (selectedApi === "clevelandMuseumArt") {
+      middleKey = "info";
+    }
+    return (
+      <p className="text-center text-sm">{artworks[middleKey].total} Results</p>
+    );
+  }
+  
   return (
     <div>
-      {artworks.data?.length > 0 && (
-        <p className="text-center text-sm">
-          {artworks.pagination.total} Results
-        </p>
-      )}
+      {renderResultsCount()}
       {artworks.data?.length > 0 &&
         artworks.data.map((artwork) => {
           return (
             <ArtworkCard
               key={artwork.id}
+              selectedApi={selectedApi}
               artworks={artworks}
               artwork={artwork}
             />
           );
         })}
       <PaginationBar
-        paginationData={artworks.pagination}
+        selectedApi={selectedApi}
+        paginationData={artworks.pagination || artworks.info}
         searchTerm={searchTerm}
         setArtworks={setArtworks}
         setLoading={setLoading}
