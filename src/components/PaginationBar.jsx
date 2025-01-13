d53ac12f097597839cf6a5b0b6f1e18885479ba3
Zvi Haffner <zvihaffner@gmail.com>
2025-01-13 18:43:47 +0000

    Adapts pagination bar to handle both APIs

--- src/components/PaginationBar.jsx
+++ src/components/PaginationBar.jsx
@@ -1,7 +1,8 @@
-import { search } from "@/chicago-api";
+import { searchChicagoArtworks, searchClevelandArtworks } from "@/api";
 import { VscEllipsis } from "react-icons/vsc";
 
 const PaginationBar = ({
+  selectedApi,
   paginationData,
   searchTerm,
   setArtworks,
@@ -11,15 +12,52 @@ const PaginationBar = ({
   function handleClick(e) {
     setError({});
     setLoading(true);
-    search(searchTerm, e.target.value, 10)
-      .then((res) => {
-        setArtworks(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setLoading(false);
-        setError(err.response.data);
-      });
+    if (selectedApi === "artInstChicago") {
+      searchChicagoArtworks(searchTerm, e.target.value, 10)
+        .then((res) => {
+          setArtworks(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+          setError(err.response.data);
+        });
+    }
+    if (selectedApi === "clevelandMuseumArt") {
+      searchClevelandArtworks(searchTerm, (e.target.value - 1) * 10, 10)
+        .then((res) => {
+          setArtworks(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+          setError({
+            status: err.response.status,
+            detail: err.response.data.detail[0].msg,
+          });
+        });
+    }
+  }
+
+  function getCurrentPage() {
+    if (selectedApi === "artInstChicago") {
+      return paginationData.current_page;
+    }
+    if (selectedApi === "clevelandMuseumArt") {
+      if (paginationData.parameters.skip === 0) {
+        return 1;
+      }
+      return paginationData.parameters.skip / 10 + 1;
+    }
+  }
+
+  function getTotalPages() {
+    if (selectedApi === "artInstChicago") {
+      return paginationData.total_pages;
+    }
+    if (selectedApi === "clevelandMuseumArt") {
+      return Math.ceil(paginationData.total / paginationData.parameters.limit);
+    }
   }
 
   return (
@@ -27,13 +65,13 @@ const PaginationBar = ({
       <button
         className="font-medium disabled:text-gray-500 disabled:cursor-not-allowed"
         onClick={handleClick}
-        value={paginationData.current_page - 1}
-        disabled={paginationData.current_page === 1}
+        value={getCurrentPage() - 1}
+        disabled={getCurrentPage() === 1}
       >
         {"< Previous"}
       </button>
 
-      {paginationData.current_page !== 1 && (
+      {getCurrentPage() !== 1 && (
         <>
           <button onClick={handleClick} value={1}>
             1
@@ -42,13 +80,13 @@ const PaginationBar = ({
         </>
       )}
       <p className="py-2 px-4 font-bold border border-black">
-        {paginationData.current_page}
+        {getCurrentPage()}
       </p>
-      {paginationData.current_page !== paginationData.total_pages && (
+      {getCurrentPage() !== getTotalPages() && (
         <>
           <VscEllipsis />
-          <button onClick={handleClick} value={paginationData.total_pages}>
-            {paginationData.total_pages}
+          <button onClick={handleClick} value={getTotalPages()}>
+            {getTotalPages()}
           </button>
         </>
       )}
@@ -56,8 +94,8 @@ const PaginationBar = ({
       <button
         className="font-medium disabled:text-gray-500 disabled:cursor-not-allowed"
         onClick={handleClick}
-        value={paginationData.current_page + 1}
-        disabled={paginationData.current_page === paginationData.total_pages}
+        value={getCurrentPage() + 1}
+        disabled={getCurrentPage() === getTotalPages()}
       >
         {"Next >"}
       </button>
