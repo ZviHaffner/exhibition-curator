c17defb184143bd5b8f8da07844a253b26b83ad3
Zvi Haffner <zvihaffner@gmail.com>
2025-01-15 17:01:44 +0000

    Adds artwork by id api calls

--- src/api.js
+++ src/api.js
@@ -13,6 +13,12 @@ export const searchChicagoArtworks = (q, page, limit) => {
   );
 };
 
+export const getChicagoArtworkById = (id) => {
+  return artInstChicagoApi.get(
+    `/${id}?fields=id,title,date_display,artist_display,description,image_id`
+  );
+};
+
 const clevelandMuseumArtApi = axios.create({
   baseURL: "https://openaccess-api.clevelandart.org/api/artworks",
 });
@@ -21,4 +27,10 @@ export const searchClevelandArtworks = (q, skip, limit) => {
   return clevelandMuseumArtApi.get("/?fields=id,title,creators,images", {
     params: { q, skip, limit },
   });
+};
+
+export const getClevelandArtworkById = (id) => {
+  return clevelandMuseumArtApi.get(
+    `/${id}?fields=id,title,date_text,creators,description,images`
+  );
 };
