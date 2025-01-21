f6daed0af0395d60fd81a439759e6e1f33cae66d
Zvi Haffner <zvihaffner@gmail.com>
2025-01-21 16:55:00 +0000

    Adds search tags to make more semantic

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -41,19 +41,21 @@ const SearchBar = ({
   }
 
   return (
-    <form onSubmit={handleSubmit} className="w-3/5 mx-auto">
-      <div className="flex gap-5 my-5 p-1 bg-white border border-gray-300 rounded-full shadow-equal">
-        <CiSearch className="text-3xl text-gray-500" />
-        <input
-          className="grow rounded-r-full focus:outline-none"
-          type="search"
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-          placeholder="Search Thousands of Artworks"
-          required
-        />
-      </div>
-    </form>
+    <search>
+      <form onSubmit={handleSubmit} className="w-3/5 mx-auto">
+        <div className="flex gap-5 my-5 p-1 bg-white border border-gray-300 rounded-full shadow-equal">
+          <CiSearch className="text-3xl text-gray-500" />
+          <input
+            className="grow rounded-r-full focus:outline-none"
+            type="search"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            placeholder="Search Thousands of Artworks"
+            required
+          />
+        </div>
+      </form>
+    </search>
   );
 };
 
