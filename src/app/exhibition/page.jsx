635a853bd790a233df4379c7d66b3786f2467035
Zvi Haffner <zvihaffner@gmail.com>
2025-01-29 18:29:06 +0000

    Styles reset button

--- src/app/exhibition/page.jsx
+++ src/app/exhibition/page.jsx
@@ -34,6 +34,7 @@ const Exhibition = () => {
             onClick={() => {
               setExhibitionArtworks([]);
             }}
+            className="mr-10 py-2 px-4 bg-white border rounded-sm shadow-equal hover:bg-gray-100 disabled:text-gray-500 disabled:cursor-not-allowed"
           >
             Reset
           </button>
