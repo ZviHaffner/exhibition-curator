6eff52253c9d57c10d7f1ea9f04f7a0b869a6973
Zvi Haffner <zvihaffner@gmail.com>
2025-02-03 16:05:41 +0000

    Basic styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="bg-red-50 border-2 border-red-600 text-center p-5">
+    <div className="text-center text-5xl p-8 font-serif">
       <Link href="/">Exhibition Curator</Link>
     </div>
   );
