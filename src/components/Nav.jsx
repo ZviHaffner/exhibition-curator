235a5a954a3e54a096ec7d4ce26fa9a7487cec27
Zvi Haffner <zvihaffner@gmail.com>
2025-02-03 16:06:16 +0000

    Adds some basic styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="bg-blue-50 border-2 border-blue-600 p-2">
+    <nav className="font-serif text-xl py-2">
       <ul className="flex text-center">
-        <li className="flex-1">
+        <li className="flex-1 hover:underline underline-offset-8">
           <Link href="/">Home</Link>
         </li>
-        <li className="flex-1">
+        <li className="flex-1 hover:underline underline-offset-8">
           <Link href="/search">Search Artworks</Link>
         </li>
-        <li className="flex-1">
+        <li className="flex-1 hover:underline underline-offset-8">
           <Link href="/exhibition">My Exhibition</Link>
         </li>
       </ul>
