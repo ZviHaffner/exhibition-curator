f26e1269d87383678fdbba2354852eeb40b163f3
Zvi Haffner <zvihaffner@gmail.com>
2025-02-08 21:57:42 +0000

    Adjusts styling to be responsive to screen size

--- src/components/ArtworkCard.jsx
+++ src/components/ArtworkCard.jsx
@@ -73,38 +73,40 @@ const ArtworkCard = ({ selectedApi, artworks, artwork }) => {
   }
 
   return (
-    <div className="flex items-center justify-between bg-white m-5 rounded-3xl shadow-lg transition duration-100 ease-in-out hover:-translate-y-1">
+    <div className="md:flex items-center justify-between bg-white my-8 rounded-3xl shadow-lg transition duration-100 ease-in-out hover:-translate-y-1">
       <Link
         href={`/artworks/${assignArtworkSrc()}/${artwork.id}`}
-        className="flex items-center"
+        className="md:flex items-center"
       >
-        <div className="flex size-32 bg-gray-50 mr-5 rounded-l-3xl">
+        <div className="flex h-48 md:size-32 md:mr-5 md:rounded-l-3xl bg-gray-50">
           {getImgSrc() ? (
             <img
               src={getImgSrc()}
               alt={artwork.title || "Artwork"}
-              className="size-32 object-cover rounded-l-3xl"
+              className="w-full h-48 rounded-t-3xl object-cover md:size-32 md:rounded-none md:rounded-l-3xl"
             />
           ) : (
             <MdOutlineImageNotSupported className="m-auto text-gray-300 size-20" />
           )}
         </div>
-        <div>
-          <p className="font-bold">
+        <section className="my-4">
+          <p className="text-center md:text-left font-bold">
             {artwork.title.length > 70
               ? artwork.title.slice(0, 70) + "..."
               : artwork.title}
           </p>
-          <p className="text-sm">{getArtists()}</p>
-        </div>
+          <p className="text-center md:text-left text-sm">{getArtists()}</p>
+        </section>
       </Link>
-      <button
-        className="mr-10 py-2 px-4 border rounded-sm shadow-equal hover:bg-gray-100"
-        value={artwork.id}
-        onClick={!isArtworkAdded() ? handleAddArtwork : handleRemoveArtwork}
-      >
-        {!isArtworkAdded() ? "Add to Exhibition" : "Remove from Exhibition"}
-      </button>
+      <div className="w-full flex justify-center md:w-auto md:mr-10">
+        <button
+          className="mb-4 md:mb-0 py-2 px-4 border rounded-sm shadow-md hover:bg-gray-100"
+          value={artwork.id}
+          onClick={!isArtworkAdded() ? handleAddArtwork : handleRemoveArtwork}
+        >
+          {!isArtworkAdded() ? "Add to Exhibition" : "Remove from Exhibition"}
+        </button>
+      </div>
     </div>
   );
 };
